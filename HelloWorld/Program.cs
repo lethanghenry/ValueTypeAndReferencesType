e824613bfd9b7ff67fb1b1dc9c3901b1c45a9fa2
thang <lethang2kth@gmail.com>
2021-12-31 11:46:08 +0700

    thang

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Welcome to NTQ");
+        }
+    }
+}
---
+++ ValueTypeAndReferrenceType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ValueTypeAndReferrenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x;
+            x = 5;
+
+            string str1;
+            str1 = "Hello there";
+
+            int a;
+            a = 1;
+            int b;
+            b = a;
+            a++;
+
+            string str2;
+            str2 = "thang";
+            string str3;
+            str3 = str2;
+
+        }
+    }
+}
